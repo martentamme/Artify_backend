de4a2c878bd4f9661104ea2bf29eeae69fa56d95
marte <martet@ttu.ee>
2021-05-01 16:05:28 +0300

    Add sectors handler

--- handlers/sectors_handler.py
+++ handlers/sectors_handler.py
@@ -1,10 +1,16 @@
 from abc import ABC
 
 import tornado.web
-import sectors
+from service import sectors
 
 
 class SectorsHandler(tornado.web.RequestHandler, ABC):
+
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET')
+
     def get(self):
         s = sectors.Sectors()
         response = s.get_sectors()
